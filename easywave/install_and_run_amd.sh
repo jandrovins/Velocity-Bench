ca6d4157066c80c581abb2b3203b8545f07c9e97
varcila <vincent.arcila@bsc.es>
2023-07-24 15:56:22 +0200

    Exit if EW_DATA does not exist

--- easywave/install_and_run_amd.sh
+++ easywave/install_and_run_amd.sh
@@ -2,7 +2,10 @@
 
 export EW_DATA=/gpfs/projects/bsc15/bsc15889/Velocity-Bench/easywave/data 
 echo "You probably need to edit variable EW_DATA, currently equals $EW_DATA"
-[ ! -d "$EW_DATA" ] && echo "Directory $EW_DATA DOES NOT exist - you need to download easywave data input from https://git.gfz-potsdam.de/id2/geoperil/easyWave/-/tree/master/data"
+if [ ! -d "$EW_DATA" ]; then
+    echo "Directory $EW_DATA DOES NOT exist - you need to download easywave data input from https://git.gfz-potsdam.de/id2/geoperil/easyWave/-/tree/master/data"
+    exit 1
+fi
 
 module load ninja/1.11.1 gcc/10.2.0 cmake/3.26.3 oneapi/2023.2.0
 . /gpfs/apps/AMD/ONEAPI/2023.0.0.25337/setvars.sh
